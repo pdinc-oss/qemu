1240dae339bb63549e718c6334584421d66e65bb
Arvin Lin <arvinlin@google.com>
2023-11-10 18:59:13 +0000

    hw/usb/redirect-host: Implement chardev handler
        Implement basic functionality of chardev frontend handlers
    
    hw/usb/redirect-host: Implement parser read/write ability
    hw/usb/redirect-host: Introduce device callback
    
        USB redirect host will be using USBRedirectHostOps to interact with the
    binded device.
    
    Google-Bug-Id: 309701600
    Change-Id: I286cebc490954dfc4d1517b65147180afbf145f3
    Signed-off-by: Arvin Lin <arvinlin@google.com>

--- hw/usb/redirect-host.c
+++ hw/usb/redirect-host.c
@@ -10,28 +10,184 @@
 #include "qemu/osdep.h"
 #include "hw/usb/redirect-host.h"
 
+#include <usbredirparser.h>
+#include <usbredirproto.h>
+
 #include "chardev/char.h"
 #include "chardev/char-fe.h"
 #include "hw/qdev-core.h"
 #include "hw/qdev-properties-system.h"
+#include "qapi/qapi-types-run-state.h"
+#include "qemu/error-report.h"
 #include "qemu/log-for-trace.h"
 #include "qemu/log.h"
 #include "qemu/typedefs.h"
+#include "qemu/units.h"
+#include "sysemu/runstate.h"
 
-static void usbredir_host_event_handler(void *opaque, QEMUChrEvent event)
+#define VERSION "qemu usb-redir host " QEMU_VERSION
+
+static void usbredir_host_parser_log(void *priv, int level, const char *msg)
 {
-    /* TODO(b/309701600): To be implemented in the following CLs. */
+    switch (level) {
+    case usbredirparser_error:
+        error_report("[ERROR] usbredir-host: %s", msg);
+        break;
+    case usbredirparser_warning:
+        error_report("[WARNING] usbredir-host: %s", msg);
+        break;
+    case usbredirparser_info:
+        error_report("[INFO] usbredir-host: %s", msg);
+        break;
+    default:
+        qemu_log_mask(LOG_TRACE, "[DEBUG] usbredir-host: %s", msg);
+    }
 }
 
-static void usbredir_host_read(void *opaque, const uint8_t *buf, int size)
+static int usbredir_host_parser_read(void *priv, uint8_t *data, int count)
 {
-    /* TODO(b/309701600): To be implemented in the following CLs. */
+    USBRedirectHost *usbredir_host = priv;
+
+    if (!usbredir_host->read_cache) {
+        return 0;
+    }
+
+    if (usbredir_host->read_size < count) {
+        count = usbredir_host->read_size;
+    }
+
+    memcpy(data, usbredir_host->read_cache, count);
+
+    usbredir_host->read_size -= count;
+    if (usbredir_host->read_size) {
+        usbredir_host->read_cache += count;
+    } else {
+        usbredir_host->read_cache = NULL;
+    }
+
+    return count;
+}
+
+static gboolean usbredir_host_parser_write_ready(void *_, GIOCondition cond,
+                                                 void *opaque)
+{
+    USBRedirectHost *usbredir_host = opaque;
+    usbredir_host->write_ready_watch = 0;
+    usbredirparser_do_write(usbredir_host->parser);
+
+    return FALSE;
+}
+
+static int usbredir_host_parser_write(void *priv, uint8_t *data, int count)
+{
+    USBRedirectHost *usbredir_host = priv;
+    int written_bytes = 0;
+
+    if (!qemu_chr_fe_backend_open(&usbredir_host->chr)) {
+        return 0;
+    }
+
+    if (!runstate_check(RUN_STATE_RUNNING)) {
+        return 0;
+    }
+
+    written_bytes = qemu_chr_fe_write(&usbredir_host->chr, data, count);
+
+    if (written_bytes < count) {
+        if (!usbredir_host->write_ready_watch) {
+            usbredir_host->write_ready_watch = qemu_chr_fe_add_watch(
+                &usbredir_host->chr, G_IO_OUT | G_IO_HUP,
+                usbredir_host_parser_write_ready, usbredir_host);
+        }
+        if (written_bytes < 0) {
+            written_bytes = 0;
+        }
+    }
+
+    return written_bytes;
+}
+
+static void usbredir_host_parser_hello(void *priv,
+                                       struct usb_redir_hello_header *h)
+{
+    USBRedirectHost *usbredir_host = priv;
+    usbredir_host->device_ops->on_attach(usbredir_host->opaque);
+}
+
+static void usbredir_host_create_parser(USBRedirectHost *usbredir_host)
+{
+    uint32_t caps[USB_REDIR_CAPS_SIZE] = {
+        0,
+    };
+
+    usbredir_host->parser = usbredirparser_create();
+
+    if (!usbredir_host->parser) {
+        error_report("%s: usbredirparser_create() failed",
+                     object_get_canonical_path(OBJECT(usbredir_host)));
+        exit(1);
+    }
+
+    usbredir_host->parser->priv = usbredir_host;
+    usbredir_host->parser->log_func = usbredir_host_parser_log;
+    usbredir_host->parser->read_func = usbredir_host_parser_read;
+    usbredir_host->parser->write_func = usbredir_host_parser_write;
+    usbredir_host->parser->hello_func = usbredir_host_parser_hello;
+
+    usbredirparser_caps_set_cap(caps, usb_redir_cap_connect_device_version);
+    usbredirparser_caps_set_cap(caps, usb_redir_cap_ep_info_max_packet_size);
+    usbredirparser_caps_set_cap(caps, usb_redir_cap_64bits_ids);
+    usbredirparser_caps_set_cap(caps, usb_redir_cap_32bits_bulk_length);
+    usbredirparser_init(usbredir_host->parser, VERSION, caps,
+                        USB_REDIR_CAPS_SIZE, usbredirparser_fl_usb_host);
+    usbredirparser_do_write(usbredir_host->parser);
+}
+
+static void usbredir_host_destroy_parser(USBRedirectHost *usbredir_host)
+{
+    if (usbredir_host->parser) {
+        usbredirparser_destroy(usbredir_host->parser);
+    }
+}
+
+static void usbredir_host_chardev_event_handler(void *opaque,
+                                                QEMUChrEvent event)
+{
+    switch (event) {
+    case CHR_EVENT_OPENED:
+        usbredir_host_create_parser(USB_REDIR_HOST(opaque));
+        break;
+    case CHR_EVENT_CLOSED:
+        usbredir_host_destroy_parser(USB_REDIR_HOST(opaque));
+    default:
+        break;
+    }
+}
+
+static void usbredir_host_chardev_read(void *opaque, const uint8_t *buf,
+                                       int size)
+{
+    USBRedirectHost *usbredir_host = USB_REDIR_HOST(opaque);
+
+    usbredir_host->read_cache = buf;
+    usbredir_host->read_size = size;
+
+    usbredirparser_do_read(usbredir_host->parser);
 }
 
 static int usbredir_host_can_read(void *opaque)
 {
-    /* TODO(b/309701600): To be implemented in the following CLs. */
-    return 0;
+    USBRedirectHost *usbredir_host = USB_REDIR_HOST(opaque);
+
+    if (!usbredir_host->parser) {
+        return 0;
+    }
+
+    if (!runstate_check(RUN_STATE_RUNNING)) {
+        return 0;
+    }
+
+    return 1 * MiB;
 }
 
 static void usbredir_host_realize(DeviceState *dev, Error **errp)
@@ -39,14 +195,21 @@ static void usbredir_host_realize(DeviceState *dev, Error **errp)
     USBRedirectHost *usbredir_host = USB_REDIR_HOST(dev);
 
     if (qemu_chr_fe_backend_connected(&usbredir_host->chr)) {
-        qemu_chr_fe_set_handlers(
-            &usbredir_host->chr, usbredir_host_can_read, usbredir_host_read,
-            usbredir_host_event_handler, NULL, usbredir_host, NULL, true);
-    }
-    else {
+        qemu_chr_fe_set_handlers(&usbredir_host->chr, usbredir_host_can_read,
+                                 usbredir_host_chardev_read,
+                                 usbredir_host_chardev_event_handler, NULL,
+                                 usbredir_host, NULL, true);
+    } else {
         qemu_log_mask(LOG_TRACE, "%s: continuing without chardev",
                       object_get_canonical_path(OBJECT(dev)));
     }
+}
+
+static void usbredir_host_unrealize(DeviceState *dev)
+{
+    USBRedirectHost *usbredir_host = USB_REDIR_HOST(dev);
+    qemu_chr_fe_deinit(&usbredir_host->chr, true);
+    usbredir_host_destroy_parser(usbredir_host);
 }
 
 static Property usbredir_host_properties[] = {
@@ -60,6 +223,7 @@ static void usbredir_host_class_init(ObjectClass *klass, void *data)
     device_class_set_props(dc, usbredir_host_properties);
     dc->desc = "USB Redirect Host";
     dc->realize = usbredir_host_realize;
+    dc->unrealize = usbredir_host_unrealize;
 }
 
 static const TypeInfo usbredir_host_info = {
--- include/hw/usb/redirect-host.h
+++ include/hw/usb/redirect-host.h
@@ -10,6 +10,8 @@
 #ifndef REDIRECT_HOST_H
 #define REDIRECT_HOST_H
 
+#include <usbredirparser.h>
+
 #include "chardev/char-fe.h"
 #include "hw/qdev-core.h"
 #include "qemu/typedefs.h"
@@ -17,9 +19,35 @@
 #define TYPE_USB_REDIR_HOST "usbredir-host"
 OBJECT_DECLARE_SIMPLE_TYPE(USBRedirectHost, USB_REDIR_HOST)
 
+/* Callback functions defined by a USB device to handle usbredir event. */
+typedef struct USBRedirectHostOps {
+    void (*on_attach)(void *opaque);
+} USBRedirectHostOps;
+
 typedef struct USBRedirectHost {
     DeviceState parent_obj;
     CharBackend chr;
+
+    const uint8_t *read_cache;
+    int read_size;
+    gint write_ready_watch;
+    struct usbredirparser *parser;
+    const USBRedirectHostOps *device_ops;
+    void *opaque;
 } USBRedirectHost;
+
+/**
+ * usbredir_host_set_ops - Sets callback functions that controls a USB device.
+ * @usbredir_host - A usbredir host to connect to a USB device
+ * @device_ops - Callback functions from the USB device
+ * @opaque - The USB device
+ */
+static inline void usbredir_host_set_ops(USBRedirectHost *usbredir_host,
+                                         const USBRedirectHostOps *device_ops,
+                                         void *opaque)
+{
+    usbredir_host->device_ops = device_ops;
+    usbredir_host->opaque = opaque;
+}
 
 #endif /* REDIRECT_HOST_H */
