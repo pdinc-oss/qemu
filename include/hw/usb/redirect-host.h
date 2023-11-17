139bb6e0245b00a0fe9b155edc94fbfabe4b003f
Arvin Lin <arvinlin@google.com>
2023-11-17 04:09:22 +0000

    hw/usb/redirect-host: Add signatures to send descriptors
    Add signatures to send endpoint, interface, and device descriptors in
    usbredir protocol format.
    
    Google-Bug-Id: 309701600
    Change-Id: Iabaf51bb2d1361ddd7aa839619bcf3c38f60a600
    Signed-off-by: Arvin Lin <arvinlin@google.com>

--- include/hw/usb/redirect-host.h
+++ include/hw/usb/redirect-host.h
@@ -11,6 +11,7 @@
 #define REDIRECT_HOST_H
 
 #include <usbredirparser.h>
+#include <usbredirproto.h>
 
 #include "chardev/char-fe.h"
 #include "hw/qdev-core.h"
@@ -48,6 +49,53 @@ static inline void usbredir_host_set_ops(USBRedirectHost *usbredir_host,
 {
     usbredir_host->device_ops = device_ops;
     usbredir_host->opaque = opaque;
+}
+
+/**
+ * usbredir_host_send_ep_info - Sends an endpoint descriptor with usbredir
+ * protocol
+ * @usbredir_host - A usbredir host to send endpoint descriptor with.
+ * @ep_info - The endpoint descriptor
+ *
+ * Returns 0 on success.
+ */
+static inline int usbredir_host_send_ep_info(
+    USBRedirectHost *usbredir_host, struct usb_redir_ep_info_header *ep_info)
+{
+    usbredirparser_send_ep_info(usbredir_host->parser, ep_info);
+    return usbredirparser_do_write(usbredir_host->parser);
+}
+
+/**
+ * usbredir_host_send_interface_info - Sends an interface descriptor with
+ * usbredir protocol
+ * @usbredir_host - A usbredir host to send interface descriptor with.
+ * @interface_info - The interface descriptor
+ *
+ * Returns 0 on success.
+ */
+static inline int usbredir_host_send_interface_info(
+    USBRedirectHost *usbredir_host,
+    struct usb_redir_interface_info_header *interface_info)
+{
+    usbredirparser_send_interface_info(usbredir_host->parser, interface_info);
+    return usbredirparser_do_write(usbredir_host->parser);
+}
+
+/**
+ * usbredir_host_send_device_connect - Sends a device descriptor with usbredir
+ * protocol
+ * @usbredir_host - A usbredir host to send device descriptor with.
+ * @device_info - The device descriptor
+ *
+ * Returns 0 on success.
+ */
+static inline int usbredir_host_send_device_connect(
+    USBRedirectHost *usbredir_host,
+    struct usb_redir_device_connect_header *device_info)
+{
+    usbredirparser_send_device_connect(usbredir_host->parser, device_info);
+    return usbredirparser_do_write(usbredir_host->parser);
 }
 
 #endif /* REDIRECT_HOST_H */
