e1cd2fb915dcf769ae7259d4c879a1a904e85ef3
Arvin Lin <arvinlin@google.com>
2024-05-01 23:27:26 +0000

    tests/qtest/npcm8xx_udc-test: Add start connection test
    Google-Bug-Id: 335724238
    Change-Id: I611ff3037611ed42b21d624cecbf90ba55bf59d7
    Signed-off-by: Arvin Lin <arvinlin@google.com>

--- tests/qtest/npcm8xx_udc-test.c
+++ tests/qtest/npcm8xx_udc-test.c
@@ -7,7 +7,10 @@
  *
  */
 
+#include <libusb-1.0/libusb.h>
+
 #include "qemu/osdep.h"
+#include "hw/usb/npcm8xx-udc.h"
 #include "fake-usbredir-guest.h"
 #include "libqtest-single.h"
 #include "socket_util.h"
@@ -25,6 +28,7 @@
 #define F_USBCMD_RESET 0x2
 
 #define R_USBSTS 0x144
+#define M_USBSTS_USB_INTERRUPT 0x1
 #define M_USBSTS_PORT_CHANGE_DETECT 0x4
 
 #define R_USBINTR 0x148
@@ -40,6 +44,8 @@
 #define F_USBMODE_BIG_ENDIANNESS 0x4
 #define F_USBMODE_SETUP_LOCKOUT_OFF 0x8
 
+#define R_ENDPTSETUPSTAT 0x1AC
+
 #define R_ENDPTCTRL0 0x1C0
 #define F_ENDPTCTRL_TX_BULK_TYPE 0x800000
 #define F_ENDPTCTRL_RX_BULK_TYPE 0x8
@@ -47,6 +53,15 @@
 typedef struct TestData {
     int sock;
 } TestData;
+
+static inline void make_control_transfer_packet(uint32_t *target,
+                                                uint8_t request_type,
+                                                uint8_t request, uint16_t value,
+                                                uint16_t index, uint16_t length)
+{
+    target[0] = request_type | (request << 8) | (value << 16);
+    target[1] = index | (length << 16);
+}
 
 static inline void npcm8xx_udc_reset(void)
 {
@@ -109,6 +124,21 @@ static inline void npcm8xx_udc_run(void)
     command = readl(NPCM8xxUDC6_BASE_ADDR + R_USBCMD);
     command |= F_USBCMD_RUN;
     writel(NPCM8xxUDC6_BASE_ADDR + R_USBCMD, command);
+}
+
+static inline void npcm8xx_udc_handle_port_connect(void)
+{
+    uint32_t port_status, usb_status;
+
+    /* Assert IRQ */
+    port_status = readl(NPCM8xxUDC6_BASE_ADDR + R_PORTSC1);
+    g_assert_true(port_status & M_PORTSC1_CONNECT_STATUS);
+    usb_status = readl(NPCM8xxUDC6_BASE_ADDR + R_USBSTS);
+    g_assert_true(usb_status & M_USBSTS_PORT_CHANGE_DETECT);
+
+    /* Clear USB interrupt status */
+    usb_status &= M_USBSTS_PORT_CHANGE_DETECT;
+    writel(NPCM8xxUDC6_BASE_ADDR + R_USBSTS, usb_status);
 }
 
 static void test_register_access(void)
@@ -184,6 +214,42 @@ static void test_run_device(const void *data)
     close(fd);
 }
 
+static void test_connect_device_port(const void *data)
+{
+    FakeUsbredirGuest faker;
+    uint32_t ep_setup_status, usb_status;
+    uint32_t expected_control_packets[2];
+    QueueHead queue_head;
+    TestData *test_data = (TestData *)data;
+    int fd = socket_util_setup_fd(test_data->sock);
+
+    fake_usbredir_guest_init(&faker, fd);
+    fake_usbredir_guest_start(&faker);
+
+    npcm8xx_udc_init();
+    npcm8xx_udc_run();
+    npcm8xx_udc_handle_port_connect();
+
+    fake_usbredir_guest_stop(&faker);
+
+    /* Verify the UDC received control transfer after handling port connect */
+    g_assert_true(get_irq(NPCM8xxUDC6_IRQ));
+    ep_setup_status = readl(NPCM8xxUDC6_BASE_ADDR + R_ENDPTSETUPSTAT);
+    g_assert_true(ep_setup_status & 1);
+    usb_status = readl(NPCM8xxUDC6_BASE_ADDR + R_USBSTS);
+    g_assert_true(usb_status & M_USBSTS_USB_INTERRUPT);
+
+    /* Verify the control transfer is a standard GET_DESCRIPTOR */
+    make_control_transfer_packet(
+        expected_control_packets, LIBUSB_ENDPOINT_IN,
+        LIBUSB_REQUEST_GET_DESCRIPTOR, LIBUSB_DT_CONFIG << 8, 0, 512);
+    memread(EP_TD_BASE_ADDR, &queue_head, sizeof(QueueHead));
+    g_assert_cmphex(queue_head.setup[0], ==, expected_control_packets[0]);
+    g_assert_cmphex(queue_head.setup[1], ==, expected_control_packets[1]);
+
+    close(fd);
+}
+
 int main(int argc, char **argv)
 {
     int ret, sock, port;
@@ -207,6 +273,8 @@ int main(int argc, char **argv)
     qtest_add_data_func("/npcm8xx_udc/attach_device", &test_data,
                         test_attach_device);
     qtest_add_data_func("/npcm8xx_udc/run_device", &test_data, test_run_device);
+    qtest_add_data_func("/npcm8xx_udc/connect_device_port", &test_data,
+                        test_connect_device_port);
 
     ret = g_test_run();
     qtest_end();
