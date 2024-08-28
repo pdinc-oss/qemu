ef171f7d348bb60303463bb57a642cbb15349472
Rezaul Barbhuiya <rezaulb@google.com>
2024-08-28 14:23:28 -0700

    Initialize the UiCommand for existing XR buttons
    The UiCommand enables to identify the command the button is
    attached to and manage the shortcuts for the command. The
    commands for existing XR buttons are not initilialized and
    thus lack proper support for enabling shortcuts or being
    identified in the ToolWindow class.
    
    Test: No visible changes.
    Bug: b/354762757
    Change-Id: I2e194c41c0fa52638db8ea64091519cbb8b27b78

--- android/android-ui/modules/aemu-ui-widgets/src/android/skin/qt/qt-ui-commands.cpp
+++ android/android-ui/modules/aemu-ui-widgets/src/android/skin/qt/qt-ui-commands.cpp
@@ -92,6 +92,9 @@ constexpr CommandInfo kCommands[] = {
         INIT_COMMAND(WEAR_2, "Wear OS Button 2"),
         INIT_COMMAND(PALM, "Palm gesture"),
         INIT_COMMAND(TILT, "Tilt gesture"),
+        INIT_COMMAND(CHANGE_XR_INPUT_MODE, "Input selector"),
+        INIT_COMMAND(CHANGE_XR_ENVIRONMENT_MODE, "Environment selector"),
+        INIT_COMMAND(XR_SCREEN_RECENTER, "Reset Position"),
 };
 
 bool parseQtUICommand(const QString& string, QtUICommand* command) {
