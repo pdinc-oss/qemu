aosp-mirror/platform_external_qemu

qom: Add diagnostic when requested to create an object which is not registered.
This is useful when configuring devices, as some CLI options call this
function blindly based on input from the CLI. Otherwise the assertion
failure is simply pointing to a nullptr which is useless.

Google-Bug-Id: 262972068
Google-Rebase-Count: 6
PiperOrigin-RevId: 395765812
Signed-off-by: Alan Jones <alanjones@google.com>
Change-Id: I57d764bca36862c558774a66882fabf03eabe69d

qom/object.c
@@ -766,6 +766,9 @@ Object *object_new(const char *type_name)
 {
     TypeImpl *ti = type_get_by_name(type_name);
 
+    if (ti == NULL) {  // GOOGLE CHANGE: diagnostics.
+        error_report("Failed to find type for '%s'\n", type_name);
+    }
     return object_new_with_type(ti);
 }
 
@@ -1100,7 +1103,7 @@ static void object_class_foreach_tramp(gpointer key, gpointer value,
         return;
     }
 
-    if (data->implements_type && 
+    if (data->implements_type &&
         !object_class_dynamic_cast(k, data->implements_type)) {
         return;
     }

pdinc-oss/qemu

qom/object.c
@@ -794,6 +794,9 @@ Object *object_new(const char *typename)
 {
     TypeImpl *ti = type_get_by_name(typename);
 
+    if (ti == NULL) {  // GOOGLE CHANGE: diagnostics.
+        error_report("Failed to find type for '%s'\n", typename);
+    }
     return object_new_with_type(ti);
 }
 
@@ -1128,7 +1131,7 @@ static void object_class_foreach_tramp(gpointer key, gpointer value,
         return;
     }
 
-    if (data->implements_type && 
+    if (data->implements_type &&
         !object_class_dynamic_cast(k, data->implements_type)) {
         return;
     }